8b35d85e22e0cffe75d7928ee716ce5fd5f00020
rawhan10 <rohanloni50@gmail.com>
2025-08-26 22:08:40 +0530

    Update tt_um_10_dco.v

--- src/tt_um_10_dco.v
+++ src/tt_um_10_dco.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_10_ihp (
+module tt_um_10_dco (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
